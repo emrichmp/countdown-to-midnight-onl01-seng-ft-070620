9aa4b686ca8f07f9e18e80a546c3591cd486420e
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-08 01:40:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@
 def countdown(num)
   while num != 0
   sleep 1
-    num -= 1
     puts "#{num} SECOND(S)!"
+    num -= 1
   end
   return "HAPPY NEW YEAR!"
 end
@@ -12,8 +12,8 @@ end
 def countdown_with_sleep(num)
   while num != 0
     sleep 1
-    num -= 1
     puts "#{num} SECOND(S)!"
+    num -= 1
   end
   return "HAPPY NEW YEAR!"
 end
